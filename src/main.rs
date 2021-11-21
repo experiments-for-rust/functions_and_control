5e2989ba5b3ea3787f1a5e29a894a76c5d2fde42
skmtseiyaku <skmtseiyaku@outlook.com>
2021-11-21 21:35:54 +0900

    Rust-style return

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ fn main() {
     let a=35;
     let b=57;
     println!("average() returns {}",average(a,b));
+    println!("average2() returns {}",average2(a,b));
 }
 
-fn average(x:i32, y:i32)->i32{
+fn average(x:i32, y:i32)->i32{ //define return data type
     return (x+y)/2;
+}
+
+fn average2(x:i32, y:i32)->i32{
+    (x+y)/2 //rust-style return
 }
