8821470815c01ae6961620a7175caa637ad58de3
skmtseiyaku <skmtseiyaku@outlook.com>
2021-11-22 23:09:23 +0900

    Added for loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
     let a=35;
     let b=28;
+
     println!("average() returns {}",average(a as f32,b as f32)); //use as to modify data type
     println!("average2() returns {}",average2(a,b));
     average3(a,b);
     println!("gcd() returns {}",gcd(a,b));
+    array_print();
 }
 
 fn average(x:f32, y:f32)->f32{ //define returned data type; notice the data type
@@ -27,5 +29,19 @@ fn gcd(x:i32, y:i32)->i32{
    }
 }
 
+fn array_print(){
+    let array1:[i32;5]=[1,2,3,4,5];
+
+    for i in array1.iter(){
+        println!("array_print() prints {}",i);
+    }
+
+    for i in (0..5).rev() {
+        println!("array_print() prints {} reversely",array1[i])
+    }
+
+    
+}
+
 //NEXT: use condition sentence in value assignment
-//match, for, loop, do while
+//match, loop, do while
