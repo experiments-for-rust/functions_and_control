8de375e1e4a440dd7e1e1c7274141d288510dbab
skmtseiyaku <skmtseiyaku@outlook.com>
2021-11-22 23:31:01 +0900

    Added io

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
     let a=35;
     let b=28;
 
     println!("average() returns {}",average(a as f32,b as f32)); //use as to modify data type
     println!("average2() returns {}",average2(a,b));
+
     average3(a,b);
+
     println!("gcd() returns {}",gcd(a,b));
+
     array_print();
+    let_if();
+
+    echo();
+
 }
 
 fn average(x:f32, y:f32)->f32{ //define returned data type; notice the data type
@@ -40,8 +50,29 @@ fn array_print(){
         println!("array_print() prints {} reversely",array1[i])
     }
 
-    
+
 }
 
-//NEXT: use condition sentence in value assignment
-//match, loop, do while
+fn let_if(){
+    let tf= false;
+    let n=if tf{
+        "TRUE"
+    }else{
+        "FALSE"
+    };
+    println!("n={}",n);
+}
+
+fn echo(){
+    print!("ECHO input: ");
+    io::stdout().flush().unwrap();
+
+    let mut echo_io:String;
+    echo_io=String::new(); //try to assign in c style
+
+    io::stdin().read_line(&mut echo_io).expect("error");
+    
+    println!("ECHO output: {}",echo_io);
+}
+
+//match, loop, while
