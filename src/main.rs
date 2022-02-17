2989a7a9c31a926d19ec78498ad1e7acf52be3f1
skmtseiyaku <skmtseiyaku@outlook.com>
2022-02-17 14:31:25 +0900

    Added 'while'

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,35 @@
 //use std::io::Write;
 
 fn main() {
-    let a=35;
+    //let a=35;
+    let a={
+        let a_temp=34;
+        a_temp+1 // return not allowed; returned value here
+    };
     let b=28;
 
     println!("average() returns {}",average(a as f32,b as f32)); //use as to modify data type
-    println!("average2() returns {}",average2(a,b));
-    average3(a,b);
+    println!("");
+    average2(a,b);
+
     println!("gcd() returns {}",gcd(a,b));
+    println!("");
+
     array_print();
     let_if();
+
     loop1();
+    我趣汉字();
 }
 
 fn average(x:f32, y:f32)->f32{ //define returned data type; notice the data type
-    return (x+y)/2.0;
+    //(x+y)/2
+    return (x+y)/2.0; //return allowed
 }
 
-fn average2(x:i32, y:i32)->i32{
-    (x+y)/2 //rust-style return
-}
-
-fn average3(x:i32, y:i32){ //void return function
-    println!("average3() directly prints {}",(x+y)/2); //also works without ;
+fn average2(x:i32, y:i32){ //void return function
+    println!("average2() directly prints {}",(x+y)/2); //also works without ;
+    println!("");
 }
 
 fn gcd(x:i32, y:i32)->i32{
@@ -38,7 +45,7 @@ fn array_print(){
     let array1:[i32;5]=[1,2,3,4,5];
     let array2=[1,1,4,5,1,4];
 
-    for i in array1.iter(){
+    for i in array1.iter(){ //iteration
         println!("array_print() prints {}",i);
     }
 
@@ -53,27 +60,26 @@ fn array_print(){
     for i in array2.iter(){
         println!("array_print() prints {}",i);
     }
+    println!("");
 }
 
 fn let_if(){
-    let tf= false;
+    let condition= false;
 
-    let n=if tf{
+    let n=if condition{
         "TRUE"
-    }else if tf{
+    }else if !condition{
         "FALSE"
     }else{
         "ERROR"
     };
 
     println!("n={}",n);
+    println!("");
 }
 
 fn loop1(){
     let out="114514";
-
-    //echo 5 times in loop
-
     let mut i=0;
 
     loop{
@@ -83,6 +89,44 @@ fn loop1(){
             break;
         }
     }
+    println!("");
+
+    i=0;
+    'counting: loop{ //label for loop
+        println!("i={}", i);
+        let mut j=10;
+
+        loop{
+            println!("j={}",j);
+            if j==9{
+                break;
+            }
+            if i==2{
+                break 'counting;
+            }
+            j-=1;
+        }
+        i+=1;
+    }
+    println!("count endded at i={}", i);
+    println!("");
+
+    i=0;
+    let r=loop{
+        i+=1;
+        if i==10{
+            break i*2;
+        }
+    };
+    println!("r={}",r);
+    println!("");
 }
 
-//while
+fn 我趣汉字(){
+    let mut i=0;
+    while i<=10{
+        println!("i={}",i);
+        i+=1;
+    }
+    println!("");
+}
